7b3b61bb3c364fae409577d9f207ed5f5bcb1b37
Sia Ivova Hranova <ivsi289a@tu-dresden.de>
2025-05-06 12:40:28 +0200

    Revert "i made a change"
    This reverts commit dd2d6a0badd605697b7b2e51b7f51fe4384a9d3c.

--- HDP_stick_breaking/3_simulate_HDP_IMM_MAB.py
+++ HDP_stick_breaking/3_simulate_HDP_IMM_MAB.py
@@ -17,7 +17,6 @@ from world import World
 plt.rcParams['figure.dpi'] = 100
 np.random.seed(8)
 
-# change
 
 def plot_heatmap(data, file_title=str(0), title=None,vmin=0,vmax=1,save=False,dpi=300, rewards=False):
     
@@ -510,7 +509,7 @@ for alpha, gamma, kappa, rho_l, rho_g in sim_params:
 
 
                 for c in range(data.K):
-                    ax.plot(post_context[:,1,c],label=f"Context {c+1}", linewidth=1.5)
+                    ax.plot(post_context[:,1,c],label=f"Context {c+1}", linewidth=2)
                 
                 ax.plot(novel_context[:,1], 'gray', label=f"Template\ncontext")
                 
